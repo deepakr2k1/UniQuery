21edc438598dbced55bd9704ed714ff3f62a0f9c
deepakr2k1 <dev.deepakr2k1@gmail.com>
2025-05-25 17:10:04 +0200

    refactore-code

--- uniquery/src/main.py
+++ uniquery/src/main.py
@@ -1,8 +1,4 @@
 from .cli import UniQueryCLI
-from .query_engine.main import QueryEngine
-from .utils import DatabaseType
-from .connection_details_manager import ConnectionDetailsManager
-from .connectors.connector import get_connection
 
 def main():
     cli = UniQueryCLI()
